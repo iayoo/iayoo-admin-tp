1086df933ee7ca7510ac553628d2eb5c7c02e728
Iayoo <vilnius94@163.com>
2021-12-28 18:02:53 +0800

    # composer新增依赖/登录修改

--- app/admin/controller/Login.php
+++ app/admin/controller/Login.php
@@ -14,15 +14,14 @@ class Login extends BaseController
     /**
      * 后台登录
      * @param AdministratorValidate $validate
+     * @param AdministratorService $administratorService
      * @return string|void
      * @throws \Exception
      */
-    public function index(AdministratorValidate $validate){
+    public function index(AdministratorValidate $validate,AdministratorService $administratorService){
         if ($this->request->isAjax()){
             $validate->scene('login')->check($this->request->param());
-            /** @var AdministratorService $service */
-            $service = app()->make(AdministratorService::class);
-            if ($service->login($this->request->param())){
+            if ($administratorService->login($this->request->param())){
                 return $this->success("登录成功");
             }
             return $this->success("登录失败");
