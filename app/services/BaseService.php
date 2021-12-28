76ca21c162dc8bd4b951fd2a6d394913d21e0755
vilnius <vilnius94@163.com>
2021-12-28 22:07:46 +0800

    # 添加修改基础类集成

--- app/services/BaseService.php
+++ app/services/BaseService.php
@@ -11,5 +11,15 @@ use Iayoo\ServiceHelper\thinkphp\ServiceProvider;
 
 class BaseService extends ServiceProvider
 {
+    public function update($where,$data){
+        return $this->model::where($where)->update($data);
+    }
 
+    public function create($data){
+        return $this->model::create($data);
+    }
+
+    public function get($data){
+        return $this->model::where($data)->find();
+    }
 }
