dbc7dfb64690802a128a698857e5b8fe6f84d6c2
Iayoo <vilnius94@163.com>
2021-12-28 16:17:39 +0800

    # 登录新增验证/接口返回格式调整

--- app/admin/controller/Login.php
+++ app/admin/controller/Login.php
@@ -5,17 +5,21 @@ namespace app\admin\controller;
 
 
 use app\services\AdministratorService;
+use app\validate\admin\Administrator as AdministratorValidate;
 use think\Request;
 
 class Login extends BaseController
 {
+
     /**
      * 后台登录
+     * @param AdministratorValidate $validate
      * @return string|void
      * @throws \Exception
      */
-    public function index(){
+    public function index(AdministratorValidate $validate){
         if ($this->request->isAjax()){
+            $validate->scene('login')->check($this->request->param());
             /** @var AdministratorService $service */
             $service = app()->make(AdministratorService::class);
             if ($service->login($this->request->param())){
