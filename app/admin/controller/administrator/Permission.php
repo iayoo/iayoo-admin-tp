a54a9243bda389734f9cbe721e150d2df9cce4f5
Iayoo <vilnius94@163.com>
2021-12-28 17:43:43 +0800

    # layui 表单模版

--- app/admin/controller/administrator/Permission.php
+++ app/admin/controller/administrator/Permission.php
@@ -16,6 +16,34 @@ class Permission extends \app\admin\controller\BaseController
             $list = $service->getList();
             return $this->setIsLayer(true)->success(['code'=>0,'data'=>$list->toArray(),'extend'=>['count' => $list->count()]]);
         }
+
+        $this->assign('datatableUrl',url(''));
+        $cols = [
+            [
+                ['field'=> 'title', 'minWidth'=> 200, 'unresize'=> true,'title'=> '权限名称'],
+                ['field'=> 'icon', 'align'=> 'center', 'unresize'=> true,'title'=> '图标','templet'=>'#icon-view'],
+                ['field'=> 'type', 'title'=> '权限类型','unresize'=> true,'templet'=>'#power-type'],
+                ['title'=> '状态', 'field'=> 'status','align'=> 'center','unresize'=> true,'templet'=> '#status','width'=> 100],
+                ['field'=> 'sort', 'title'=> '排序','unresize'=> true],
+                ['title'=> '操作','templet'=> '#power-bar', 'unresize'=> true,'width'=> 150, 'align'=> 'center']
+            ]
+        ];
+        $renderConfig = [
+            'treeColIndex'     => 0,
+            'treeSpid'         => 0,
+            'treeIdName'       => 'id',
+            'treePidName'      => 'pid',
+            'skin'             => 'line',
+            'method'           => 'post',
+            'treeDefaultClose' => false,
+            'toolbar'          => '#power-toolbar',
+            'elem'             => '#power-table',
+            'url'              => request()->url(),
+            'page'             => false,
+            'cols'             => $cols
+        ];
+        $this->assign('renderConfig',json_encode($renderConfig));
+
         return $this->fetch();
     }
 
