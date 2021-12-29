fb3ef660fb11ab044465c09e00197e47c60f1d40
Iayoo <vilnius94@163.com>
2021-12-29 11:09:11 +0800

    # 菜单权限验证器

---
+++ app/validate/admin/AdministratorPermission.php
@@ -0,0 +1,34 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\validate\admin;
+
+use think\Validate;
+
+class AdministratorPermission extends Validate
+{
+    protected $failException = true;
+    /**
+     * 定义验证规则
+     * 格式：'字段名' =>  ['规则1','规则2'...]
+     *
+     * @var array
+     */
+    protected $rule = [
+        'title|名称' => 'require',
+        'type|类型' => 'require',
+        'sort|排序' => 'require|between:1,99',
+    ];
+
+    /**
+     * 定义错误信息
+     * 格式：'字段名.规则名' =>  '错误信息'
+     *
+     * @var array
+     */
+    protected $message = [];
+
+    protected $scene = [
+        'create'=>['title|名称','type|类型','sort|排序']
+    ];
+}
