97f693776e7432f5a841a0b95865a0dc99f7120b
Iayoo <vilnius94@163.com>
2021-12-29 11:46:09 +0800

    # 角色权限中间表定义

---
+++ app/model/AdministratorRolePermission.php
@@ -0,0 +1,15 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\model;
+
+use think\Model;
+use think\model\Pivot;
+
+/**
+ * @mixin \think\Model
+ */
+class AdministratorRolePermission extends Pivot
+{
+    //
+}
