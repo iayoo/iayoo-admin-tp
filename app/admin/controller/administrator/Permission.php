58a95403aaeeb95c8d10f1df7a4842b65697c934
Iayoo <vilnius94@163.com>
2021-12-29 11:10:03 +0800

    # 新增菜单创建功能

--- app/admin/controller/administrator/Permission.php
+++ app/admin/controller/administrator/Permission.php
@@ -8,6 +8,8 @@ namespace app\admin\controller\administrator;
 
 
 use app\services\AdministratorPermissionService;
+use app\services\ToolService;
+use app\validate\admin\AdministratorPermission;
 
 class Permission extends \app\admin\controller\BaseController
 {
@@ -49,5 +51,33 @@ class Permission extends \app\admin\controller\BaseController
 
     public function getList(){
         return $this->success();
+    }
+
+    public function add(AdministratorPermissionService $service)
+    {
+        return $this->fetch('',[
+            'permissions' => ToolService::getTree($service->getList())
+        ]);
+    }
+
+    public function save(AdministratorPermissionService $service)
+    {
+        $params = $this->request->param();
+        $validate = new AdministratorPermission();
+        $validate->scene('create')->check($params);
+        if ($service->create($params)){
+            return $this->success("保存成功");
+        }
+        return $this->error('保存失败');
+//        //验证
+//        $validate = new V;
+//        if(!$validate->check($data))
+//            return ['msg'=>$validate->getError(),'code'=>201];
+//        try {
+//            M::create($data);
+//            rm();Session::clear();
+//        }catch (\Exception $e){
+//            return ['msg'=>'操作失败'.$e->getMessage(),'code'=>201];
+//        }
     }
 }
