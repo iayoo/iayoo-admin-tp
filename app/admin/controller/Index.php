c486bb75faf7e2a55929c1cbdf16e2fdc13901a8
vilnius <vilnius94@163.com>
2022-01-01 12:16:47 +0800

    # 合并样式文件

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -48,4 +48,8 @@ class Index extends BaseController
         }
         return $this->error("操作失败");
     }
+
+    public function loading(){
+        echo '<svg stroke="#000" width="50px" height="50px" viewBox="0 0 38 38" style="transform:scale(0.8);" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><g transform="translate(1 1)" stroke-width="2"><circle stroke-opacity=".25" cx="18" cy="18" r="18"></circle><path d="M36 18c0-9.94-8.06-18-18-18"><animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="1s" repeatCount="indefinite"></animateTransform></path></g></g></svg>';
+    }
 }
